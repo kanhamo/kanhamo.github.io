a5726ac38bc735ea0fa1384257c7daef2f4935ef
oyama4pgv <oyama@pgv.co.jp>
2022-01-29 17:16:19 +0900

    スライダーテスト

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+    autoplay: true,         //自動再生
+    autoplaySpeed: 2000,    //自動再生のスピード
+    speed: 800,             //スライドするスピード
+    dots: true,             //スライドしたのドット
+    arrows: true,          //左右の矢印
+    infinite: true,         //スライドのループ
+    pauseOnHover: false,   //ホバーしたときにスライドを一時停止しない　
+});
